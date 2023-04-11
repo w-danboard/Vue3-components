d0db115fc31095e67af705377489b91cb2ae8cc7
王琳 <wanglin@idss-cn.com>
2023-04-11 16:26:01 +0800

    feat:(搭建monorepo环境)

---
+++ typings/vue-shime.d.ts
@@ -0,0 +1,8 @@
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// 这个目的就是在编写引入.vue文件的时候，能识别出组件的类型
